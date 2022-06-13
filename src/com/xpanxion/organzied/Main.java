0f1c08a4619580e1ca836d54ebf37a10a552336a
Chi Sio <chisio@xpanxion.com>
2022-06-13 15:25:15 -0400

    Hello world1

--- src/com/xpanxion/organzied/Main.java
+++ src/com/xpanxion/organzied/Main.java
@@ -3,7 +3,7 @@ package com.xpanxion.organzied;
 public class Main {
     public static void main(String[] args){
         System.out.println("First at \\line\\");
-        System.out.println("Seconds at \\line\\");
+        System.out.println("Second at \\line\\");
 
     }
 }
