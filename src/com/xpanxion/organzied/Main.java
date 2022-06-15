3128eb3e16f8c4235dc287eebf933d97e913b6a2
Chi Sio <chisio@xpanxion.com>
2022-06-15 12:14:31 -0400

    Conditional statement

--- src/com/xpanxion/organzied/Main.java
+++ src/com/xpanxion/organzied/Main.java
@@ -1,9 +1,30 @@
 package com.xpanxion.organzied;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("First at \\line\\");
-        System.out.println("Second at \\line\\");
+        Scanner scan = new Scanner(System.in);
+        double x;
+        double y;
+        double z;
 
+        System.out.println("Enter for x: "  );
+        x = scan.nextDouble();
+        System.out.println("Enter for y: " );
+        y = scan.nextDouble();
+        System.out.println("Enter for z: " );
+        z = scan.nextDouble();
+
+        if ((x < y) && (y < z)){
+            System.out.println("INCREASING");
+        }
+        else if ((x > y) && (y > z)){
+
+            System.out.println("DECREASING");
+        }
+        else {
+            System.out.println("NEITHER");
+        }
     }
 }
