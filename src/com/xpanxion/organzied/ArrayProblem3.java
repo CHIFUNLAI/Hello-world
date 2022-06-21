9a0265d38a8d8615b51562b127e35b3db9f4ef06
Chi Sio <chisio@xpanxion.com>
2022-06-21 00:02:31 -0400

    array

--- src/com/xpanxion/organzied/ArrayProblem3.java
+++ src/com/xpanxion/organzied/ArrayProblem3.java
@@ -2,20 +2,25 @@ package com.xpanxion.organzied;
 
 public class ArrayProblem3 {
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8,9,10};
+        int[] a = {2,3,9,17,32,40,73,40,21,10};
 
-        int temp = 0;
-        temp = a[9];
-        a[9]= 1;
-        a[8] = temp;
-        int[] b = new int[10];
+        int temp = a[0];
+
+        System.out.println("After shifting: ");
+
         for (int i = 0; i< a.length; i++){
-
+            System.out.print(a[i] + " ");
         }
 
+        System.out.println("");
 
+        System.out.println("After shifting: ");
 
-
-
+        for (int i = 0; i< a.length; i++){
+            if (i >= 0 && i < 9) {
+                System.out.print(a[i + 1] + " ");
+            }
+        }
+        System.out.print(temp);
     }
 }
